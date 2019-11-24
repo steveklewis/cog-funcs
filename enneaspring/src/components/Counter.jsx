91d139ecf5c31d7442289c5c8ae2627ff67ef51e
steveklewis <lewisksteven@gmail.com>
2019-11-23 22:31:53 -0600

    Add counter component from https://scotch.io/tutorials/create-animated-react-apps-with-react-spring. Delay by one second.

--- enneaspring/src/components/Counter.jsx
+++ enneaspring/src/components/Counter.jsx
@@ -4,6 +4,7 @@ import { Spring } from 'react-spring/renderprops';
 
 const counter = () => (
     <Spring
+       delay='1000'
        from={{number: 0}}
        to={{number: 10 }}>
        {props => <div>{props.number.toFixed()}</div>}
