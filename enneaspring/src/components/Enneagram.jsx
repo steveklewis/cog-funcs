87d12959a2be4438331cf3700cfc19683abc4ba8
steveklewis <lewisksteven@gmail.com>
2019-12-07 11:17:12 -0600

    Get text for 8, 9, 1, filling out on hover.

--- enneaspring/src/components/Enneagram.jsx
+++ enneaspring/src/components/Enneagram.jsx
@@ -5,8 +5,9 @@ import {useSpring, animated } from 'react-spring'
 const Enneagram = (props) => {
 const colorProps = useSpring({stroke: "white", from: {stroke: "blue"}, delay: '2000'});
 
+let oneText = props.angerActive ? '1 - Over-controls Anger' : '8';
 const oneProps = useSpring({
-    x: !props.angerActive ? "77.1345131623847" : "150",
+    x: !props.angerActive ? "77.1345131623847" : "330",
     y: !props.angerActive ? "-91.9253331742774" : "-90",
     fontSize: props.angerActive ? "30pt": "18pt"
 });
@@ -27,22 +28,25 @@ const fourProps = useSpring({
     fontSize: props.shameActive ? "30pt": "18pt"
 });
 
+let eightText = props.angerActive ? '8 - Over-expresses Anger' : '8';
+
 const eightProps = useSpring({
-    x: !props.angerActive ? "-77.1345131623847" : "150",
+    x: !props.angerActive ? "-77.1345131623847" : "345",
     y: !props.angerActive ? "-91.9253331742774" : "-40",
     fontSize: props.angerActive ? "30pt": "18pt"
 });
 
+let nineText = props.angerActive ? '9 - Avoids Anger' : '9';
+
 const nineProps = useSpring({
-    x: !props.angerActive ? "0" : "150",
+    x: !props.angerActive ? "0" : "280",
     y: !props.angerActive ? "-120" : "10",
     fontSize: props.angerActive ? "30pt": "18pt",
     message: props.angerActive ? "9 - Avoids Anger" : "9"
 });
 
-debugger;
 
-return <animated.svg xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://purl.org/dc/elements/1.1/" xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" width="500" height="300" stroke={colorProps.stroke}>
+return <animated.svg xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://purl.org/dc/elements/1.1/" xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" width="700" height="300" stroke={colorProps.stroke}>
 
 <title>Enneagram</title>
 <p class="whitetext"> {props.explain}</p>
@@ -70,15 +74,15 @@ return <animated.svg xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://pu
 
 
 <g transform="translate(0,9)">
-<animated.text x={oneProps.x} y={oneProps.y} font-size={oneProps.fontSize} text-anchor="middle" font-family="serif">1</animated.text>
+<animated.text x={oneProps.x} y={oneProps.y} font-size={oneProps.fontSize} text-anchor="middle" font-family="serif">{oneText}</animated.text>
 <animated.text x={twoProps.x} y="-20.8377813200316" font-size={twoProps.fontSize} text-anchor="middle" font-family="serif">2</animated.text>
 <animated.text x={threeProps.x} y="60" font-size={threeProps.fontSize} text-anchor="middle" font-family="serif">3</animated.text>
 <animated.text x={fourProps.x} y="112.763114494309" font-size={fourProps.fontSize} text-anchor="middle" font-family="serif">4</animated.text>
 <text x="-41.0424171990803" y="112.763114494309" font-size="18pt" text-anchor="middle" font-family="serif">5</text>
 <text x="-103.923048454133" y="60" font-size="18pt" text-anchor="middle" font-family="serif">6</text>
 <text x="-118.176930361465" y="-20.8377813200316" font-size="18pt" text-anchor="middle" font-family="serif">7</text>
-<animated.text x={eightProps.x} y={eightProps.y} font-size={eightProps.fontSize} text-anchor="middle" font-family="serif">8</animated.text>
-<animated.text x={nineProps.x} y={nineProps.y} font-size={nineProps.fontSize} text-anchor="middle" font-family="serif">{nineProps.message}</animated.text>
+<animated.text x={eightProps.x} y={eightProps.y} font-size={eightProps.fontSize} text-anchor="middle" font-family="serif">{eightText}</animated.text>
+<animated.text x={nineProps.x} y={nineProps.y} font-size={nineProps.fontSize} text-anchor="middle" font-family="serif">{nineText}</animated.text>
 </g>
 
 
