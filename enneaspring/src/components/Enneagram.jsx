977498997175959dfff44333896dc74ea37ab15b
steveklewis <lewisksteven@gmail.com>
2019-12-07 11:53:00 -0600

    Start getting the shame stuff moving correctly.

--- enneaspring/src/components/Enneagram.jsx
+++ enneaspring/src/components/Enneagram.jsx
@@ -5,21 +5,24 @@ import {useSpring, animated } from 'react-spring'
 const Enneagram = (props) => {
 const colorProps = useSpring({stroke: "white", from: {stroke: "blue"}, delay: '2000'});
 
-let oneText = props.angerActive ? '1 - Over-controls Anger' : '8';
+let oneText = props.angerActive ? '1 - Over-controls Anger' : '1';
 const oneProps = useSpring({
     x: !props.angerActive ? "77.1345131623847" : "330",
     y: !props.angerActive ? "-91.9253331742774" : "-90",
     fontSize: props.angerActive ? "30pt": "18pt"
 });
 
-
+let twoText = props.shameActive ? '2 - Over-controls Shame': '2';
 const twoProps = useSpring({
-    x: !props.shameActive ? "118.176930361465" : "150",
+    x: !props.shameActive ? "118.176930361465" : "330",
+    y: !props.shameActive ? "-20.8377813200316" : "-90",
     fontSize: props.shameActive ? "30pt": "18pt"
 });
 
+let threeText = props.shameActive ? '3 - Avoids Shame': '3';
 const threeProps = useSpring({
-    x: !props.shameActive ? "103.923048454133" : "90.1345131623847",
+    x: !props.shameActive ? "103.923048454133" : "275",
+    y: !props.shameActive ? "60" : "-40",
     fontSize: props.shameActive ? "30pt": "18pt"
 });
 
@@ -41,8 +44,7 @@ let nineText = props.angerActive ? '9 - Avoids Anger' : '9';
 const nineProps = useSpring({
     x: !props.angerActive ? "0" : "280",
     y: !props.angerActive ? "-120" : "10",
-    fontSize: props.angerActive ? "30pt": "18pt",
-    message: props.angerActive ? "9 - Avoids Anger" : "9"
+    fontSize: props.angerActive ? "30pt": "18pt"
 });
 
 
@@ -75,8 +77,8 @@ return <animated.svg xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://pu
 
 <g transform="translate(0,9)">
 <animated.text x={oneProps.x} y={oneProps.y} font-size={oneProps.fontSize} text-anchor="middle" font-family="serif">{oneText}</animated.text>
-<animated.text x={twoProps.x} y="-20.8377813200316" font-size={twoProps.fontSize} text-anchor="middle" font-family="serif">2</animated.text>
-<animated.text x={threeProps.x} y="60" font-size={threeProps.fontSize} text-anchor="middle" font-family="serif">3</animated.text>
+<animated.text x={twoProps.x} y={twoProps.y} font-size={twoProps.fontSize} text-anchor="middle" font-family="serif">{twoText}</animated.text>
+<animated.text x={threeProps.x} y={threeProps.y} font-size={threeProps.fontSize} text-anchor="middle" font-family="serif">{threeText}</animated.text>
 <animated.text x={fourProps.x} y="112.763114494309" font-size={fourProps.fontSize} text-anchor="middle" font-family="serif">4</animated.text>
 <text x="-41.0424171990803" y="112.763114494309" font-size="18pt" text-anchor="middle" font-family="serif">5</text>
 <text x="-103.923048454133" y="60" font-size="18pt" text-anchor="middle" font-family="serif">6</text>
